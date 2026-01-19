cff41b2e78f009bc4728d394515350c5828a6754
hunain-strugbits <hunainhyder@strugbitsglobal.com>
2026-01-19 18:57:41 +0500

    fix: Enable SSL for production database connection

--- src/db/db.service.ts
+++ src/db/db.service.ts
@@ -10,7 +10,11 @@ class DatabaseService {
   private constructor() {
     const pool = new pg.Pool({
       connectionString: process.env.DATABASE_URL,
-    })
+      ssl:
+        process.env.NODE_ENV === 'production'
+          ? { rejectUnauthorized: false }
+          : undefined,
+    });
 
     const adapter = new PrismaPg(pool)
 
